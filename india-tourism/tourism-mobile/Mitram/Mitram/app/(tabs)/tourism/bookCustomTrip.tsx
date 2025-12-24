8e24d842fbefc53ff03853286087d86a7acc0b71
subhankar roy <roy.cognizant.subhankar623@gmail.com>
2025-12-23 21:54:47 -0500

    IndiaTours product

---
+++ india-tourism/tourism-mobile/Mitram/Mitram/app/(tabs)/tourism/bookCustomTrip.tsx
@@ -0,0 +1,20 @@
+import { ScrollView, Text,TextInput,View } from 'react-native';
+import LoginSignUpStyle from '../../styles/loginsignup.js'; 
+import CardStyle from '../../styles/cards.js'; 
+import ProductStyle from '../../styles/productStyle.js';
+import TourCommonStyle from '../../styles/tourCommonStyle.js'; 
+import TextStyle from '../../styles/textStyles.js' ;
+import BookingStyle from '../../styles/bookingStyle.js';
+import {getBookingsByBookingId} from "../../admin/admin.js";
+import { useEffect, useState } from "react";
+import { TouchableOpacity} from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useLocalSearchParams } from 'expo-router';
+
+export default function BookCustomTrip(){
+    return(
+        <View>
+            <Text>Book your custom trip here !!</Text>
+        </View>
+    )
+}
